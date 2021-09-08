d89d7317470e982749efe2839ef586e1b7ee7fdd
Adi Skop <adiskop@gmail.com>
2021-09-08 11:25:17 +0200

    add column user_id to Dishes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_041510) do
+ActiveRecord::Schema.define(version: 2021_09_08_085507) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_09_08_041510) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "cuisine_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
