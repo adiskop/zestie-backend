7db814d6d62da3f3bc31491f53d028cc5905a1b2
Adi Skop <adiskop@gmail.com>
2021-09-08 07:19:47 +0200

    create comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_112013) do
+ActiveRecord::Schema.define(version: 2021_09_08_041510) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "dish_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "cuisines", force: :cascade do |t|
     t.string "country"
