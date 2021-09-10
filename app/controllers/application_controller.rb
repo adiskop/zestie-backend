7b384daaedf9f83daf105c5ff5c0301fcd2652d9
Adi Skop <adiskop@gmail.com>
2021-09-10 07:27:59 +0200

    include cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::API
-
-    include ::ActionDispatch::Cookies
-    
+    include ::ActionController::Cookies
         def current_user
-            User.find_by(id: session[:user_id])
+           User.find_by(id: session[:user_id])
         end
 
         def logged_in?
