d2caf6574e0dd38cf639cb4b8de9de2af4bfbadb
Adi Skop <adiskop@gmail.com>
2021-09-10 07:29:03 +0200

    add delete route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post "/api/v1/login", to: "api/v1/sessions#create"
+  get "/api/v1/signup", to: "api/v1/users#get_current_user"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   #create name spacing like so: zestie.com/api/v1/resource
   namespace :api do
