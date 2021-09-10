18919fc8f5b3d6bdf7c756212781d54932d47bb8
Adi Skop <adiskop@gmail.com>
2021-09-10 07:27:27 +0200

    add delete action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,24 +1,32 @@
 class Api::V1::SessionsController < ApplicationController
     def create
-        @user = User.find_by(username: params[:session][:username])
-        if @user && @user.authenticate(params[:session][:password])
-            session[:user_id] = @user.id
-            render json: @user
-        else
-            render json: {
-                error: "Invalid Credentials"
-            }
-        end
+      @user = User.find_by(username: params[:session][:username])
+  
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        render json: UserSerializer.new(@user), status: :ok
+      else
+        render json: {
+          error: "Invalid Credentials"
+        }
+      end
     end
-
+  
     def get_current_user
-        if logged_in?
-            
-            render json: current_user
-        else
-            render json: {
-                error: "No logged in user"
-            }
-        end
+      if logged_in?
+        render json: UserSerializer.new(current_user)
+      else
+        render json: {
+          error: "No one logged in"
+        }
+      end
     end
-end
+  
+    def destroy
+      session.clear
+      render json: {
+        notice: "successfully logged out"
+      }, status: :ok
+    end
+  
+  end
