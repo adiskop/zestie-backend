d4c7fbb01374f2611673f81d304c464247652ad7
Adi Skop <adiskop@gmail.com>
2021-09-11 15:06:29 +0200

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
     has_many :cuisines
     has_many :dishes
     has_many :comments
+
+    validates :name, :username, presence: true
+    validates :username, uniqueness: true
 end
