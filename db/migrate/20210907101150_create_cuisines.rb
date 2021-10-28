f69c17da86d6921d1fc1c2c24f3353b69645a894
Adi Skop <adiskop@gmail.com>
2021-10-28 15:56:09 +0200

    fix issue login

--- db/migrate/20210907101150_create_cuisines.rb
+++ db/migrate/20210907101150_create_cuisines.rb
@@ -1,8 +1,8 @@
 class CreateCuisines < ActiveRecord::Migration[6.1]
   def change
     create_table :cuisines do |t|
-      t.text :country
-      t.text :picture
+      t.string :country
+      t.string :picture
 
       t.timestamps
     end
--- db/migrate/20210907111759_create_dishes.rb
+++ db/migrate/20210907111759_create_dishes.rb
@@ -1,12 +1,12 @@
 class CreateDishes < ActiveRecord::Migration[6.1]
   def change
     create_table :dishes do |t|
-      t.text :name
-      t.text :picture
-      t.text :ingredients
-      t.text :directions
-      t.text :cook_time
-      t.text :yield
+      t.string :name
+      t.string :picture
+      t.string :ingredients
+      t.string :directions
+      t.string :cook_time
+      t.string :yield
 
       t.timestamps
     end
