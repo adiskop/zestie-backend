e64e9131cd01c386924c7603e88469c3d3b07a0d
Adi Skop <adiskop@gmail.com>
2021-11-02 12:24:56 +0100

    add origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://zestie.herokuapp.com/'
+    origins 'https://zestie-backend.herokuapp.com/'
 
     resource '*',
       headers: :any,
